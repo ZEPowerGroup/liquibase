b435ffa5170d3a529d986e9b108465305b892e15
wesley <wesley@ze.com>
2012-05-16 09:54:59 -0700

    Fixed case sensitivity bug in visitor filtering

--- liquibase-core/src/test/java/liquibase/changelog/filter/ContextChangeSetFilterTest.java
+++ liquibase-core/src/test/java/liquibase/changelog/filter/ContextChangeSetFilterTest.java
@@ -1,10 +1,33 @@
 package liquibase.changelog.filter;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import liquibase.changelog.ChangeSet;
+import liquibase.database.Database;
+import liquibase.sql.visitor.AbstractSqlVisitor;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ContextChangeSetFilterTest {
+
+    private static final class TestSqlVisitor extends AbstractSqlVisitor {
+
+        public TestSqlVisitor(final String... contexts) {
+            setContexts(new HashSet<String>(Arrays.asList(contexts)));
+        }
+
+        public String modifySql(String sql, Database database) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("modifySql has not been implemented");
+        }
+
+        public String getName() {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("getName has not been implemented");
+        }
+
+    }
 
     @Test
     public void emptyContexts() {
@@ -70,5 +93,53 @@ public class ContextChangeSetFilterTest {
         assertTrue(filter.accepts(new ChangeSet(null, null, false, false,  null, "test3, test1", null)));
         assertTrue(filter.accepts(new ChangeSet(null, null, false, false,  null, "test3, TEST1", null)));
         assertTrue(filter.accepts(new ChangeSet(null, null, false, false,  null, null, null)));
+    }
+
+    @Test
+    public void visitorContextFilterLowerLower() {
+        ContextChangeSetFilter filter = new ContextChangeSetFilter("test1");
+
+        ChangeSet changeSet = new ChangeSet(null, null, false, false, null, null, null);
+        changeSet.addSqlVisitor(new TestSqlVisitor("test1"));
+
+        assertTrue(filter.accepts(changeSet));
+
+        assertEquals(1, changeSet.getSqlVisitors().size());
+    }
+
+    @Test
+    public void visitorContextFilterUpperLower() {
+        ContextChangeSetFilter filter = new ContextChangeSetFilter("TEST1");
+
+        ChangeSet changeSet = new ChangeSet(null, null, false, false, null, null, null);
+        changeSet.addSqlVisitor(new TestSqlVisitor("test1"));
+
+        assertTrue(filter.accepts(changeSet));
+
+        assertEquals(1, changeSet.getSqlVisitors().size());
+    }
+
+    @Test
+    public void visitorContextFilterUpperUpper() {
+        ContextChangeSetFilter filter = new ContextChangeSetFilter("TEST1");
+
+        ChangeSet changeSet = new ChangeSet(null, null, false, false, null, null, null);
+        changeSet.addSqlVisitor(new TestSqlVisitor("TEST1"));
+
+        assertTrue(filter.accepts(changeSet));
+
+        assertEquals(1, changeSet.getSqlVisitors().size());
+    }
+
+    @Test
+    public void visitorContextFilterLowerUpper() {
+        ContextChangeSetFilter filter = new ContextChangeSetFilter("test1");
+
+        ChangeSet changeSet = new ChangeSet(null, null, false, false, null, null, null);
+        changeSet.addSqlVisitor(new TestSqlVisitor("TEST1"));
+
+        assertTrue(filter.accepts(changeSet));
+
+        assertEquals(1, changeSet.getSqlVisitors().size());
     }
 }
